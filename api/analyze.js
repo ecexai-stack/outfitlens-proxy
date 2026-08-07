8f39abd78115e07920e9db06780cba4f247c55c7
ece <aleeshashahid047@gmail.com>
2026-08-07 09:30:30 +0500

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -22,4 +22,4 @@ module.exports = async function handler(req, res) {
     return res.status(500).json({ error: err.message });
   }
 }
-module.exports.config = { api: { bodyParser: { sizeLimit: '50mb' } } };
+module.exports.config =  { api: { bodyParser: { sizeLimit: '50mb' } } };
